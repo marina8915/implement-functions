0bf6385eb5f54083b3baa857947fcfa378087686
marina <marina_bakum@mail.ru>
2018-01-21 15:20:05 +0200

    fib

--- dist/bundle.js
+++ dist/bundle.js
@@ -131,7 +131,21 @@ console.log(factorial(6))                        // 720
 /* 3 */
 /***/ (function(module, exports) {
 
+function fib(n) {
+    var rez = 0
+    if (n <= 1) {
+        rez = n
+    } else {
+        rez += fib(n - 1) + fib(n - 2)
+    }
+    return rez
+}
 
+console.log('fib')
+console.log(fib(0))                              // 0
+console.log(fib(1))                              // 1
+console.log(fib(10))                             // 55
+console.log(fib(20))                             // 6765
 
 /***/ })
 /******/ ]);
